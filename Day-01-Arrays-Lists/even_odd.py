d1820ad03483f0f05b189d35ccc2803115bf0778
Aakansha <tembhurneaakansha@gmail.com>
2026-09-16 14:43:04 +0530

    Find even and odd numbers in a list

---
+++ Day-01-Arrays-Lists/even_odd.py
@@ -0,0 +1,13 @@
+numbers = [10, 15, 20, 25, 30, 35]
+
+print("Even numbers:")
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+print("Odd numbers:")
+
+for num in numbers:
+    if num % 2 != 0:
+        print(num)

0096e5b143b043e470181fa64f74e4915af2ad99
Aakansha <tembhurneaakansha@gmail.com>
2026-09-18 16:50:08 +0530

    Add Bubble Sort

---
+++ Day-03-Sorting/bubble_sort.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 8]
+
+for i in range(len(numbers)):
+    for j in range(len(numbers) - 1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print(numbers)
